66b7709f3f0f01faaf008e49af928be18fd651ac
isabellacpmelo <isacpmelo@gmail.com>
2025-05-17 11:29:25 -0300

    feat: Component properties (props) and children in React

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá, mundo!
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illum
         ipsa, nam, laudantium excepturi exercitationem ipsam pariatur ad,
