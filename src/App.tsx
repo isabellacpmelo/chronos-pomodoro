ce63564f45d7856dbd4e861d529e8852949754f7
isabellacpmelo <isacpmelo@gmail.com>
2025-05-19 15:23:07 -0300

    feat: Creating the home page form

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,27 @@ function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>task</label>
+            <input type='text' id='input' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <div>0 0 0 0 0 0 0</div>
+          </div>
+
+          <div className='formRow'>
+            <button type='button'>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
