6fe9460d52ecb1116d6fae0921b884cfd0edbcd7
isabellacpmelo <isacpmelo@gmail.com>
2025-05-31 15:35:15 -0300

    feat: Global application state with useState and communication via props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,27 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
 
+const inicialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 function App() {
-  return <Home />;
+  const [state, setState] = useState(inicialState);
+
+  console.log('App', state);
+
+  return <Home state={state} setState={setState} />;
 }
 
 export { App };
