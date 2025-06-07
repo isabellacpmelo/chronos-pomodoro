f02d79612a517b892287ee4c6b7cb6043462c7f8
isabellacpmelo <isacpmelo@gmail.com>
2025-06-07 11:02:16 -0300

    feat: Changing the start button to stop the current task

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -66,6 +66,7 @@ export function MainForm() {
           labelText='task'
           placeholder='Digite algo'
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
