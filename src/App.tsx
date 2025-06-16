7dbbc78675cc489030f12ec5ed6f24da3180e2f7
isabellacpmelo <isacpmelo@gmail.com>
2025-06-16 14:46:50 -0300

    feat: React Toastify with adapter - reusable messages the right way

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
 
 import './styles/theme.css';
 import './styles/global.css';
+import { MessagesContainer } from './components/MessagesContainer';
 
 function App() {
   return (
     <TaskContextProvider>
-      <Home />
+      <MessagesContainer>
+        <Home />
+      </MessagesContainer>
     </TaskContextProvider>
   );
 }
